e819571d1d34257d1a61ddb91c3b4aaa700c3a35
yog27ray <yog27ray@gmail.com>
2023-11-09 06:05:06 +0000

    Build: (821dbe0) Update routes.ts

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerRoutes = void 0;
 const parse_server_generator_1 = require("./parse-server-generator");
 function registerRoutes(app) {
+    app.get('/api/health', (req, res) => res.json({ message: 'success' }));
     // parse route
     app.use('/api/parse', parse_server_generator_1.ParseServerGenerator.generate());
 }
