821dbe0d7133a96603e0d5c7549bbb2989ce57ea
yog27ray <yog27ray@gmail.com>
2023-11-09 11:34:33 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -3,6 +3,7 @@ import { Express } from 'express';
 import { ParseServerGenerator } from './parse-server-generator';
 
 function registerRoutes(app: Express): void {
+  app.get('/api/health', (req, res) => res.json({ message: 'success' }));
   // parse route
   app.use('/api/parse', ParseServerGenerator.generate());
 }
